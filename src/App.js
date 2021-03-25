d24f14e56addc9c2b5e795e0a688d7747cd13357
edwardASavage <15561454@students.lincoln.ac.uk>
2021-03-25 14:39:17 +0000

    Update image import example.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ImportImage from '../public/images/image.png'
 import "./style.scss";
 
 const App = () => {
     return (
         <div className="app">
             <h1>Hello world Two</h1>
+            <img src={ImportImage}></img>
         </div>
     );
 };
